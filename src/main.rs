dc0c96695d8b1f164ceada90b1898e400956c7b4
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2023-07-20 21:45:41 +0200

    restructured project

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::error::Error;
-
-use axum::{routing::get, Router, Server};
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let app = Router::new().route("/", get(root));
-
-    Server::bind(&"0.0.0.0:80".parse()?)
-        .serve(app.into_make_service())
-        .await?;
-
-    Ok(())
-}
-
-async fn root() -> &'static str {
-    "Hello 👋"
+fn main() {
+    if let Err(err) = planner_http::run() {
+        eprintln!("Error running http server: {err}");
+    }
 }
