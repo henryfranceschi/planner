e751f32004dfa21c724b6097ed5edd7d48398bf2
Henry Franceschi <138119852+henryfranceschi@users.noreply.github.com>
2023-12-04 23:03:14 +0100

    added wip authorization trait

--- domain/src/action.rs
+++ domain/src/action.rs
@@ -1,7 +1,14 @@
+use async_trait::async_trait;
 use diesel_async::AsyncPgConnection;
+use uuid::Uuid;
+
+use crate::{error::DomainError, model::User};
 
 pub mod project;
 pub mod task;
+
+// TODO: currently many actions only require an id, an issue with this is that it is very easy
+// to pass the wrong type of id. Think about possible solutions to this problem.
 
 pub struct Context<'a> {
     conn: &'a mut AsyncPgConnection,
@@ -11,4 +18,18 @@ impl<'a> Context<'a> {
     pub fn new(conn: &'a mut AsyncPgConnection) -> Self {
         Self { conn }
     }
+
+    pub fn user(&self) -> Option<&User> {
+        todo!()
+    }
+}
+
+pub enum Operation {
+    Read,
+    ReadWrite,
+}
+
+#[async_trait]
+pub trait Allow {
+    async fn allow(ctx: &mut Context<'_>, id: Uuid, op: Operation) -> Result<(), DomainError>;
 }
